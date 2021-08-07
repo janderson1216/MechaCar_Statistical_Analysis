0b861ebd29f242aa99f73bc6a77776f1bb8f3821
Janelle Anderson <janelleanderson@Janelles-MacBook-Air-4.local>
2021-08-06 22:31:49 -0400

    adding deliverable 1

---
+++ MechaCarChallenge.R
@@ -0,0 +1,8 @@
+install.packages("tidyverse")
+library(dplyr)
+csv.read(MechaCar_mpg.csv)
+read.csv(MechaCar_mpg.csv)
+MechaCar_mpg <- read.csv(file = 'MechaCar_mpg.csv',check.names=F, stringsAsFactors=F)
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data=MechaCar_mpg)
+)
